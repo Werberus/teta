722d8ed6b683f66ebf9cda66cb4f026bf5e7d053
Werberus <rusyak777@list.ru>
2022-10-14 23:30:10 +0300

    added sorting

--- task1/main.go
+++ task1/main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	fmt.Println(f("sasdgassd"))
-
+	fmt.Println(f("aZЯAaaBBЯz"))
 }
 
 func f(str string) (outStr string) {
-	m := make(map[rune]int)
+	m := make(map[string]int)
+	var charSlice []string
 	for _, r := range str {
-		m[r] += 1
+		char := string(r)
+		if _, ok := m[char]; !ok {
+			charSlice = append(charSlice, char)
+		}
+		m[char] += 1
 	}
-	for k, v := range m {
-		outStr += string(k) + strconv.Itoa(v)
+	sort.Strings(charSlice)
+	for _, v := range charSlice {
+		outStr += v + strconv.Itoa(m[v])
 	}
 	return
 }
